c90e338374592462f296eb9b4885db6763500cd5
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-06-23 16:39:56 +0530

    2 commit

---
+++ lecture_03.py
@@ -0,0 +1,156 @@
+
+## Mathematical Operators:
+# + -> addition
+# - -> subtraction
+# / -> division
+# % -> modulo
+
+
+# dynamic
+# a = input("bfjhfb")
+
+# # hardcode
+# a = 2
+# b = 2
+
+# # + , -, *, /, **
+
+
+# div = a  2
+# print('sub: ',sub)
+
+
+
+# mul
+# mul = a*b
+# print('sub is ', mul)
+
+
+
+
+
+# If else condition:
+
+# Operators:
+#   =    -> assignment operator = used for assigning value to a varaible
+#   ==   -> for camparision
+#   >    -> greater than
+#   <    -> less than
+#   >=   -> greater than equal to
+#   <=   -> less than equal to
+#   or   -> logical OR
+#   and  -> logical AND
+#   &    -> Bitwise AND
+#   |    -> Bitwise OR
+
+
+"""
+Q2. Write a python program for assigning values based on marks obtained by a student.
+	- if the percentage is above 95, assign grade A
+	- if the percentage is above 70 and below 95 including, assign grade B
+	- if the percentage is above 60 and below 70 including , assign grade C
+        - if the percentage is 45, assign "NEED IMPROVEMENT"
+"""
+
+
+# hardcode
+# marks = 72
+
+# # dynamic
+# percentage = int(float(input("Enter your marks: ")))
+
+# print(type(percentage))
+
+# # > 95 -> 96.....
+# # >= 95....
+
+# if (percentage > 95):
+#      print("A")
+
+# elif (percentage>70) or (percentage<=95):
+#      print("B")
+
+# elif (percentage > 60) and (percentage<=70):
+#       print("C")
+
+# elif (percentage == 45): 
+#       print("NEED IMPROVEMENT")
+
+
+
+# a = 10
+# a == "shoaib"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Q.2 write a python program considering the problem statement like:
+    
+- Ask user's their Sex / Gender.
+- Ask user's age.
+- if user is 'male' assign (Kid, Teenager, Adult):
+        - if age is above 18 then assign Adult.
+        - if age is between (11 to 19) then assign teenager.
+        - if age is below 12 then assign kid.
+- if female:
+        - if age is above 18 then assign Adult.
+        - if age is between (11 to 19) then assign teenager.
+        - if age is below 12 then assign kid.
+
+"""
+
+# gender = input("your gender is: ")
+# age = int(float(input("Enter your age: ")))
+
+# print(type(gender))
+# print(gender)
+
+# if gender == "male":
+
+#         if (age > 18):  # 19......
+#                 print("ADULT")
+
+#         elif (age > 11) & (age<19):   #12....18
+#                 print('TEENAGER')
+
+#         else:   # 1...11
+#                 print('KID')
+    
+
+# elif gender == 'female':
+
+#         if (age > 18):  # 19......
+#                 print("ADULT")
+
+#         elif (age > 11) & (age<19):   #12....18
+#                 print('TEENAGER')
+
+#         else:   # 1...11
+#                 print('KID')
+
+# else:
+#       print('transgender')
+
+
+
+# print(vbhjcdjbf)
+
+
+
+
+
