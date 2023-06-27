201fc00e2fae6508b8000725fb4244e6b278fb9c
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-06-27 15:07:43 +0530

    7 commit

---
+++ lecture_05.py
@@ -0,0 +1,73 @@
+
+# Problem Solution in class:
+"""
+Q1. Write a python program for a situation say:
+ 	- Ask user for a positive number, If the number is positive then:
+ 		- if the number is divisible by 2 then:
+			print('It is divisible by 2')
+ 		- similar do check for if the number is divisible by 
+        (3, 4, 5 and 7).
+	
+ 	- But if the user input number is a negative number then:
+ 		 print('User plz enter a positive number')
+	
+ 	- Also don't forget to check for zero.
+"""
+
+
+# num = float(input("enter value:"))
+
+
+# if num > 0:
+
+# 	print('positive')
+    
+# 	if num % 2 == 0:
+# 		print('Divisible by 2')
+
+# 	elif num % 3 == 0:
+# 		print('Divisible by 3')
+
+# 	elif num % 5 == 0:
+# 		print('Divisible by 5')
+
+# 	elif num % 7 == 0:
+# 		print('Divisible by 7')
+
+# 	elif num % 4 == 0:
+# 		print('Divisible by 4')
+
+
+# 	# print(122+122)
+# elif num < 0:
+# 	print(' number is negative')	
+
+
+
+
+# else:
+# 	print(' it is zero')	
+    
+
+
+
+
+
+
+
+
+
+
+"""
+Q2. Check whether a given number is even or odd
+"""
+
+
+# # check whether 12 is divisible by 2 or not  
+# a = float(input("Enter value: "))
+
+# if a % 2 == 0:
+# 	print("even")
+
+# elif a % 2 != 0:
+# 	print("odd")
