e98cc25f029e481ef20a4b0d03eb59745c61a5cd
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-06-30 14:39:26 +0530

    11 commit

--- lecture_07.py
+++ lecture_07.py
@@ -3,7 +3,6 @@
 2. Question.
 """
 
-
 """
 Q1: Write python program to accept the electric units from user 
 and calculate the bill according to the following criteria:
@@ -15,6 +14,38 @@ next 200 units is Rs 2 per day
 Above 300 units is Rs 5 per day
 
 """
+
+unit = float(input("Enter ur unit: "))
+amount = 0
+print(f'Your unit utilized: {unit} unit ')
+
+if (unit < 100):
+    print(f'your amount is: {amount}Rs ')
+
+elif ( 100 <= unit <= 300):
+
+    amount = 0 + unit*2 
+    
+    print(f'your amount is: {amount}Rs ')
+
+elif ( unit > 300):
+
+    amount = 0 + (200*2) + (unit-300) * 5
+
+    print(f'your amount is: {amount}Rs ')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
