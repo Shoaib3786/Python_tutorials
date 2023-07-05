64b5c21cf0ffc00afd4757fffc6fc40cad9cfe5f
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-07-05 21:18:37 +0530

    14 commit

---
+++ lecture_10.py
@@ -0,0 +1,175 @@
+### Objective ###
+"""
+1. List 
+
+2. List built-in methods:
+
+    1. append() - add in end
+    2. insert() - specify location
+    3. remove() - specify element name
+    4. pop()    - use index
+    5. slicing [:] - print ample of specified contained at once.
+    6. len()    - find length of the list
+    7. index()  - to find index of specific element and even specify limit till which u want to find
+    8. sort()   - sort element in ascending order.
+
+"""
+
+
+# declare list:
+# mtd-1
+# mlist = [12,23, 43, 'a', 'hello']
+# print(mlist)
+
+# # [or]
+
+# # mtd-2
+# a = list( (12,23, 43, 'a', 'hello') )
+# print(a)
+
+
+
+######## Opertaions #######
+
+# mlist = [12,23, 43, 'a', 'hello']
+
+# 1. append() - add in end
+# a = [20, 22, 34] 
+# print(a)
+
+# a.append(12223343233233)
+# print(a)
+
+
+#  2. insert() - specify location
+# a = [20, 22, 34] 
+
+# a.insert(1, 123455)
+
+# print(a)
+
+
+# 3. remove() - specify element name
+# a = [20, 'max',  22, 34] 
+# print(a)
+
+# a.remove('max')
+# print(a)
+
+
+# 4. pop()    - use index
+# a = [20, 'max',  22, 34] 
+# a.pop(1)
+# print(a)
+
+
+
+# 5. slicing [:] - print ample of specified contained at once.
+
+
+# 6. len()    - find length of the list
+# a = [20, 'max',  22, 34] 
+
+
+#  7. index()  - to find index of specific element and even specify limit till which u want to find
+
+# a = [20, 'max',  22, 34] 
+# print(a.index('max'))
+
+
+
+
+
+
+# a = [20, 'max',  22, 34] 
+
+# backtracing:::
+# value = a[-4:1]
+# print(value)
+
+
+
+
+
+
+
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# for x in list2:
+#   list1.append(x)
+
+# print(list1)
+
+
+
+
+# 8. sort()   - sort element in ascending order.
+
+# a = [12, 3, 43, 332324, 4545,5,5,7,8,7665]
+# print(a)
+# a.sort()
+
+# print(a)
+
+
+
+
+
+"""
+Q.2 Write python program that accepts age of 4 users and find the
+youngest user out of it.
+"""
+
+mlist = []
+
+for _ in range(4):
+    age = int(float(input('Enter age; ')))
+
+    mlist.append(age)
+
+    # print(mlist)
+
+print(mlist)
+
+# sorting in ascending order 
+mlist.sort()
+
+print(mlist)
+
+
+print(f"youngest person's age is: {mlist[0]}")
+    
+
+
+
+
+
+
+# mlist = []
+# for _ in range(5):
+    
+#     age = int(float(input('enter value: ')))
+    
+#     mlist.append(age)
+
+# print('user added list is: ',mlist)
+
+# mlist.sort()
+# print(f'Final list {mlist} \n')
+
+
+# youngest = mlist[0]
+# print(f'Youngest person age is: {youngest}', )
+
+
+
+
+
