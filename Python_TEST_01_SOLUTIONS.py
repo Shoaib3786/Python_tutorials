2a35f80009b9d69ed6b406ba8eac0cc4640473da
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-07-19 15:08:26 +0530

    24 commit

---
+++ Python_TEST_01_SOLUTIONS.py
@@ -0,0 +1,122 @@
+
+"""
+Q.5 Write the workings of the program that accepts the age 
+of 7 users and find the 2 different youngest users out of it.
+""" 
+
+
+# user = []
+# for i in range(7):
+
+#     a = int(float(input('Enter user age: ')))
+
+#     user.append(a)
+
+# print('Unsorted list are: ',user, end='\n'*2)
+
+
+
+# # sorting the user
+# user.sort()
+# print('sorted list are: ', user, end='\n'*2)
+
+
+
+
+
+# for i in user:
+
+#     first = user[0]   # 1, 1
+
+    # sec = i # 1, 2
+
+#     if first != sec:
+#         print(f'Youngest users age are : {first} & {sec}')
+        
+#         break
+
+   
+#     continue
+
+    
+
+
+
+
+"""
+Q6. Write program working to check the sum of three
+elements (each from the tree arrays.
+if Sum of each elements from three arrays is equal to a target value.
+then, Print all those three-element combinations.
+
+Sample data:
+X = [10, 20, 20, 20]
+Y = [10, 20, 30, 40]
+Z = [10, 30, 40, 20]
+target = 70
+
+"""
+
+# x = [10, 20, 20, 20]
+# y = [10, 20, 30, 40]
+# z = [10, 30, 40, 20]
+# target = 70
+
+
+# val = []
+# for i in x:   # 10
+
+#     for j in y: # 10, 20
+
+#         for k in z: # 10
+
+#             if i + j + k == target:
+
+#                 val.append(list((i,j,k)))
+
+
+# print(val)
+
+
+# print(' total combinations are: ',len(val))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# x = [10, 20, 20, 20]
+# y = [10, 20, 30, 40]
+# z = [10, 30, 40, 20]
+# val = []
+# for i in x:
+
+#     for j in y:
+
+#         for k in z:
+
+#             if i+j+k == 70:
+                
+#                 val.append((i,j,k))
+
+#             continue
+
+# print('List of elements cominations: ',val, end='\n'*2)
+
+# print('Total combinations:', len(val))
+
+
+
+
+
+
+
+
+
