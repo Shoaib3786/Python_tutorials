f66b05a498a4a9fa5d4e80eccafa6b488acc2895
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-07-24 10:26:34 +0530

    29 commit

---
+++ lecture_15 Solutions.py
@@ -0,0 +1,43 @@
+"""
+Q1. Write a python program to print the following pattern:
+
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+"""
+
+row = 5
+for i in range(1, row+1):
+    
+    for j in range(1, row-i+1):
+
+        print(" ", end= " ")
+
+    for k in range(1, i+1):
+        print("*", end=" ")
+
+    print('\n')
+
+
+
+
+"""
+Q2. Write a python program to print the following pattern:
+1
+2 2
+3 3 3
+4 4 4 4 
+5 5 5 5 5
+"""
+
+row = 5
+
+for i in range(1, row+1):
+
+    for j in range(1, i+1):
+
+        print(f"{i}", end=' ')
+
+    print('\n')
