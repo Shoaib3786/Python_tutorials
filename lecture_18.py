b79f7508e022e8a1e80941162bde03c1824992f0
Shoaib3786 <mohammad.shoaib3786@gmail.com>
2023-08-04 13:56:38 +0530

    33 commit

---
+++ lecture_18.py
@@ -0,0 +1,198 @@
+
+# Q1
+# a = 20
+# print(a)
+
+
+# Q2
+
+# datatypes: str - "hello" 
+# , float  -2.3
+# , int - 2 
+# , boolean - True/ False
+
+# a_var = 124
+# print(type(a_var))
+
+# b_var = 234.98
+# print(type(b_var))
+
+# c_var = 'hello world'
+# print(type(c_var))
+
+# d_var = True
+# print(type(d_var))
+
+
+
+# Q3
+
+# 1.
+# a = 110
+# b = 123
+
+# c = a+b
+# print(c)
+
+# 2. 
+# b  =1200
+# a = 213
+
+# c = b-a
+
+# print(c)
+
+# 3. 
+# a = 10
+# b = 5
+
+# c = a * b
+
+# print(c)
+
+# 4. 
+
+# a = 12
+# b = 3
+
+# c  = a/b
+
+# print(c)
+
+# 5. 
+# a = 3
+# b = 5
+
+# c = a ** b
+
+# print(c)
+
+# 6
+# a = 144
+
+# mtd - 1
+# import math
+
+# b = math.sqrt(a)
+
+# print(b)
+
+# mtd-2
+# b = a ** (1/2)
+# print(b)
+
+# 7.
+# a = 5
+# b = 2
+
+# c = a % b
+# print(c)
+
+
+# Q4
+
+# age = 18
+# gender = 'male'
+
+
+# if gender == 'male':
+#     if (age >= 18 ):
+#         print('adult')
+
+
+
+#     elif (12 <= age <= 18):
+#         print('teenager')
+
+
+#     else:
+#         print('kid')
+
+
+
+# elif gender == 'female':
+
+#     if (age >= 18 ):
+#         print('adult')
+
+
+
+#     elif (12<=age < 18):
+#         print('teenager')
+
+
+#     else:
+#         print('kid')
+
+
+
+# Q5
+
+# a = "Hello Poul how are you !!!"
+# print(a)
+
+
+# Q6
+
+# a = "123"
+
+# b = int(a)  # typecasting
+
+# print(type(b))
+
+# b = 123.435
+
+# c = 21    # int  21
+# d  = str(c)  # string - "21"
+
+# print( type( d ) )
+
+# Q7
+
+# purchase = 300
+
+# discounted_val  = 10/100
+
+# discount_value = purchase * discounted_val  # 300 * 10/100
+
+# payment  = purchase - discount_value
+
+# print(payment)
+
+# Q8
+
+# 1. 
+# int_val = int(float(input('Enter value: ')))
+
+# print(int_val)
+
+# # 2.
+# str_val = input('Enter value: ')
+
+# print(int_val)
+
+# 3.
+# float_val = float(input('Enter value: '))
+
+# print(float_val)
+
+# Q9.
+# mlist = [12, 34, 55, "12", "show", 1111, 12, 43]
+
+# print(mlist)
+
+# Q10
+
+# for i in range(3, 26):
+#     print(i)
+
+
+# # Q11
+
+# mlist = [12, 34, 55, "12", "show", 1111, 12, 43]
+
+# # 1
+
+# mlist.insert(3, 'Max')
+
+# print(mlist)
---
+++ lecture_19.py
@@ -0,0 +1,51 @@
+"""
+1. String operations
+2. What is whitespace
+"""
+
+
+
+
+"""
+1. concatinate operation ( + ) -> merge 
+"""
+
+
+"""
+2. multiply the string ( * )
+"""
+
+
+"""
+3. string slicing
+"""
+
+
+"""
+4. case changing ( upper(), lower() )
+"""
+
+
+"""
+5. white space removal  ( stip() )  removes element from 
+    leading and ending.
+"""
+a = ".,,,,,rrttgg.....banana....rrr,,,,"
+b = a.strip(",r.")
+print(b)
+
+
+"""
+6. splitting the string ( split() )  -> split the string n form list
+"""
+
+"""
+7. looping the string
+"""
+
+
+"""
+8. replacing the element in the string. (replace())
+"""
+
+
