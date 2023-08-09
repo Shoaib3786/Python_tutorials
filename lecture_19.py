7231048842801b95e32a99dae16fcbb8b76f74cb
Shoaib <mohammad.shoaib3786@gmail.com>
2023-08-09 14:09:34 +0530

    33 commit

--- lecture_19.py
+++ lecture_19.py
@@ -6,38 +6,118 @@
 
 
 
+
+
+
 """
 1. concatinate operation ( + ) -> merge 
 """
+
+# a = int(input('Enter value'))
+# b = int(input('Enter value'))
+# c = a+b 
+
+# a = 's'
+# b = 'd'
+
+# c = "c"
+# print( c )
+
+
+# c = " c"
+# print( c )
+
+
+# c = "   c"
+# print( c )
+
 
 
 """
 2. multiply the string ( * )
 """
 
+# a = "*"
+
+# print( a * 10)
+
+
+
+
+
+
+
+
+
 
 """
 3. string slicing
 """
+
+# list
+m = [1,2,34, 32, 22]
+# m[1: 3] # slicing
+# print(m[1: 3])
+
+# string
+# a =  '  hello world!! bye word!! '
+
+# b = a[  :  ]
+# print(b)
+
+
+
+
+
+
+
+
+
 
 
 """
 4. case changing ( upper(), lower() )
 """
 
+# a = ' HELLO WORLD !!'
+
+# b = ' bye.....'
+
+# print( a[1: 5] + b )
+
+
+
+
+
 
 """
 5. white space removal  ( stip() )  removes element from 
     leading and ending.
 """
-a = ".,,,,,rrttgg.....banana....rrr,,,,"
-b = a.strip(",r.")
-print(b)
+
+
+# a = ".,,,,,rrttgg.....banana....rrr,,,||,,,,"
+
+# print(a, '\n')
+
+# print( a.strip('|,.r') )
+
+
+
+# b = a.strip(",r.")
+# print(b)
 
 
 """
 6. splitting the string ( split() )  -> split the string n form list
 """
+
+# a = ' Hello world!!!! '
+
+# print( a.split( 'l' ) )
+
+
+
 
 """
 7. looping the string
@@ -47,5 +127,10 @@ print(b)
 """
 8. replacing the element in the string. (replace())
 """
+
+a  = ' Hello!!!!!world'
+
+print(a, '\n')
+print(a.replace('!!!!!', ' '))
 
 
