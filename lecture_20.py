5bd9f406601fe050c986b10ca5aa7f5ceebbeb77
Shoaib <mohammad.shoaib3786@gmail.com>
2023-08-09 16:18:11 +0530

    34 commit

---
+++ lecture_20.py
@@ -0,0 +1,215 @@
+
+
+"""
+OBJECTIVE:
+
+1.  String Join function
+2.  While loop
+"""
+
+
+
+
+# 1.  String Join function
+# 
+# a = " HelloPeople!!"
+
+# # a.split() # ['hello', 'people!!']
+
+# # a.split(' ')  #[' ', 'hello', ' ', 'people!!']
+
+# # print(a.replace(" ", '$$'))
+
+# symb = "$$"
+
+# print( symb.join( a.split('  ') ))
+
+
+
+
+# mconnect = ' ### '
+
+
+# print( mconnect.join( a.split(" ")))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. While loop
+
+
+""" Syntax """
+
+# while (condition):
+
+    # code
+
+# code
+
+
+
+
+
+
+
+# while True:
+
+    # code that needs to be done!!
+
+
+
+
+
+
+
+
+# Example:
+
+
+"""
+1. Write a python program to print a statement 10 times in
+"""
+
+# i = 1
+
+# while i <= 10:
+
+#     i = i + 1
+#     print("Sorry!! I won't repeat that")
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+# statement = " Sorry maam I won't repeat that again... "
+
+# i = 0
+
+# while i < 10:
+
+#     print(statement) 
+
+#     i += 2
+
+
+
+""" 2. Write a python program to print first 10 natural number """
+
+# i = 0
+# while i < 10:
+
+#     i = i+1
+#     print(i)
+
+
+
+
+
+
+# i = 0
+# while i < 11:
+
+#     print(i)
+
+#     i += 1
+
+
+
+""" 
+3. While loop with break, continue, pass statement, write program to 
+print number from (1 to 20) but:
+
+if the number 10 appears break the program,
+if number 7 appear then continue and,
+if 3 appears then don't perform anything.
+"""
+
+
+# i = 1
+
+# while i <= 20:  # 10
+
+
+#     if i == 10:
+
+#         break
+
+#     elif (i == 7):
+#         i += 1
+#         continue    
+    
+
+#     elif( i== 3):
+
+#         pass
+
+    
+#     print(i)
+#     i += 1
+
+    
+    
+a = " Welcome \t to my blog"
+
+print(a)
+
+
+
+
+
+
+
+# a = 1
+
+# while 1 <= a < 20:
+
+
+#     if a == 10:
+#         break
+
+#     elif ( a == 7):
+
+#         a += 1
+#         continue
+
+#     elif (a == 3):
+#         pass
+
+
+#     print(a)
+
+#     a += 1
+
+    
+
+
