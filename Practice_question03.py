22a8758c68cc4f1e79ca70104d9cba8c0977a976
Shoaib <mohammad.shoaib3786@gmail.com>
2023-09-02 14:12:35 +0530

    46 commit

---
+++ Practice_question03.py
@@ -0,0 +1,19 @@
+""" WHILE LOOP """
+
+"""
+Q1. Write a python program using WHILE LOOP to print this sequence of numbers:
+105, 98, 91,....,7. 
+
+
+Q2. Write a python program to print the sum of first 10 natural numbers 
+using WHILE LOOP. 
+
+
+Q3. Write a python program to print all the EVEN NUMBERS between the two
+the user input values. Use WHILE LOOP in this question.
+
+
+Q4. Write a python program to take 10 numbers from the user and print
+the largest number out of it. Use WHILE LOOP in this question as well.
+
+"""
