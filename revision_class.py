91c8f16424ef54192cd2e575f811e33e307ddd8b
Shoaib <mohammad.shoaib3786@gmail.com>
2023-09-11 12:03:51 +0530

    48 commit

--- revision_class.py
+++ revision_class.py
@@ -13,32 +13,124 @@ DISCUSSION:
 """
 
 
-
-
 #variable:
 my_var = 12
 
 a = 20
 
-b = "hello"
+c = "hello"
 
-c = True
-
-
+d = True
 
 
 
+# Print
+"""
+When to use: 
+
+- printing the variable values.
+- printing the sentences.
+- printing debugging.
+
+"""
+
+# a = " Bye people!!!!!"
+
+# print( a )
 
 
 
 
 
 
+# TAKE USER INPUT:
+
+# - Hardcoding
+# - Duynamic
+
+# a = 12  # haercoded
+
+# b = input("Please enter value: ") # dynamic
+
+# print("Value is: ",b)
 
 
 
 
 
+# TYPE-CASTING:
+    # - finding the datatype of the varaible.
+    # - casting the varaible datatype.
+    # - understanding input().
+
+
+# int - 12, 45, 99999999999
+# float - 12.54, 67.85757
+# str - "12", "hebuybuf", 'hey'
+# bool - True / False , 1/0, High/Low
+
+
+# 1. type() -> for finding datatypes of the variable.
+
+# a = '123'
+# b = type(a)
+# print(b)
+
+# a = 12.98
+# b = str(int(a))
+# print("type is: ",type(b))
+
+# print("original:", a)
+
+# b = str(a) # float -> int
+
+# print("After type casting: ",b)
+
+
+# calculator
+
+
+# Hardcoding
+# a = 10
+# b = 20
+
+# print(a+b)
+
+
+# Dynamic
+# a  = int( float( input("Enter value a; ") ))
+# b = int(float(input('Enter value b: ')))
+
+# print(a+b)
+
+
+# add = +
+# sub = -
+# multi = *
+# division = /
+
+
+
+# s1 = '10'
+# a = int(s1)
+
+# s2 = '20'
+# b = int(s2)
+
+# print(a+b) 
+
+
+
+
+"""
+Q1. typecaste hardcoded(Progammer feeded) value into:
+	- int
+	- float
+	- string
+	The value is -> 49, 49.67, "Tom Cruise"
+"""
+
+###################################
 
 
 
@@ -52,6 +144,12 @@ c = True
 
 # a = rajat
 # print(a)
+
+
+
+
+
+
 
 
 
@@ -99,7 +197,7 @@ Q1. typecaste hardcoded(Progammer feeded) value into:
 # typecaste str to int
 
 
-###### input statement #####
+##### input statement #####
 
 
 # enter your age, add 2 years in user's age and print it
@@ -130,6 +228,64 @@ DISCUSSIONS:
 1. structure of if else condition is 'if' and ends with 'else' 
 elif always comes in between. 
 """
+
+
+
+#comaprison operators:
+# <
+# >
+# >=
+# <=
+# =  -> assignment operator
+#  == -> comparison
+# and -> T T -> T
+
+
+
+
+
+
+
+
+
+
+
+# age = int(input("Enter your age:  "))
+
+
+# if else condition
+
+# if (age >= 18):
+
+#     print('Adult')
+
+# elif   (16<=age<=18):
+
+#     print('He is idot')
+    
+# elif( 12<age<16):
+
+#     print('paagal')
+
+# else:
+#     print('Kid')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # if (condition):
@@ -189,22 +345,106 @@ of the city name:
 #       print('None')
 
 
-
- 
 """
-Q4. Write python program to check whether a person is senior citizen 
-or not:
-	- if person's age > 60 then he/she is a senior citizen
-	- else he/she is not.
+Q3. Accept any city name from the user and display the monuments 
+of the city name:
+ - if city is delhi then print monument red fort
+ - if city is Agra then print monument Taj Mahal
+ - if city is Jaipur then print monument Jal Mahal
 """
 
-# age = int(float(input("Enter age: ")))
 
-# if age >= 60:
-#     print('Senior citizen')
-    
+"""
+Q5. Write a python program for assigning grades 
+(A, B, C, D, E, F, G) based on marks obtained by a student.
+	- if the percentage is above 95, assign grade A
+	- if the percentage is above 70 and below and including 95, assign grade B
+	- if the percentage is above 60 and below and including 70, assign grade C	
+    - if the percentage is between (50, 60), assign D
+    - if the percentage is (40 to 50), assign E
+    - if the percentage is 29 and 30 , assign F
+    - if the percentage is 10 or 11, assign G 
+"""
+
+
+# per = int(input('Enter you marks: '))
+# # per2 = int(input('Enter you marks: '))
+
+# if (per > 95):
+#     print('A')
+
+# elif ( 70<per<=95):
+#     print('B')
+
+# elif(60<per<=70):
+#     print('C')
+
+# elif (50<per<60):
+#     print('D')
+
+# elif(40<=per<=50):
+#     print('E')
+
+# elif (per==29) and (per == 30):
+#     print('F')
+
+# elif (per==10) or (per ==11):
+#     print('G')
+
+
 # else:
-#     print('none')
+#     print('No grades')
+
+
+
+"""
+Q9: Write a python program, A shop will give discount of 10% 
+    - if the cost of purchased quantity is more than 1000 bucks. 
+Also,
+- Ask user for quantity [Suppose, one unit will cost 100 bucks].
+- Judge and print total cost for user.
+"""
+
+
+
+"""
+Q10: Write python program to accept the electric units from user 
+and calculate the bill according to the following criteria:
+
+First 100 units is Free
+
+next 200 units is Rs 2 per unit
+
+Above 300 units is Rs 5 per unit
+"""
+ 
+
+
+# unit =  int(input("Enter your electric units: "))
+
+# amount = 0
+
+# if (unit <= 100):
+#     amount = 0 
+#     print('Your amount is: ', amount)
+
+# elif (100 < unit <= 300):
+
+#     amount = 0 + (unit-100) * 2
+#     print('Your amount is: ', amount)
+
+# else:
+
+#     amount= 0 + 200*2 + (unit-300) * 5
+#     print('Your amount is: ', amount)
+
+
+
+
+
+
+
+
 
 
 
@@ -360,7 +600,6 @@ First 100 units is Free
 next 200 units is Rs 2 per unit
 
 Above 300 units is Rs 5 per unit
-
 """
 
 
@@ -392,13 +631,10 @@ Above 300 units is Rs 5 per unit
 ### FOR LOOP ###
 ################
 
-
 """
 DISCUSSION:
 1. For loop
     - Iteration -> no.of times to loop 
-
-3. Minor List concept
     - Index -> Location
 
 2. range() funtion
@@ -406,23 +642,43 @@ DISCUSSION:
 
 
 
+
+
+
+
+
+
+
+
 """
 Q11. Print first 10 natural number.
 """
 
-# for i in range(1, 11):
 
+# print(1,2,3,4,5,6,7,8,9,10)
+
+# print(1)
+# print(2)
+# print("...", end='\n')
+# print(10)
+
+
+# for i in range(1, 11):
 #     print(i)
 
+# print('last i is: ',i)
 
 
 """
 Q12. Print numbers from -10 to -1 using for loop.
 """
 
-# for i in range(-10, 0, 2):
+# for i in range(-10, 0,  1):
 
 #     print(i)
+
+
+
 
 
 """
@@ -430,48 +686,31 @@ Q13. make variables for start and end points for looping and
 printing the values.
 """
 
+# start = 1
+# end = 11
+# jump = 1
 
-# my_start = int(input('enter value'))
-# my_end = int(input('enter value'))
-# my_jump = int(input('enter value'))
-
-# for i in range( my_start, my_end, my_jump):
+# for i in range(1, 11, -1):
 
 #     print(i)
 
 
 
-"""
+
+"""_
 Q find the sum of first 10 numbers:
 """
 
+# print(0+1+2+3+4+5+6+7+8+9)
+
 # sum = 0
-
-# for i in range(1, 11):
-
+# for i in range(10):
+    
 #     sum = sum + i
 
-
-#     print(sum)
-
+# print(sum)
 
 
-
-"""
-Q14: Find the factorial of 2, 5, 6, 7 & 8.
-"""
-
-# 5! = 5 * 4 * 3 * 2 * 1 = 120
-
-
-# s = 1
-
-# for i in range(1,6):
-
-#     fact = s * i
-#     s = fact
-
-# print(fact)
 
 
 
@@ -637,8 +876,8 @@ to a user input numbers.
 
 
 
-mlist = [1,23,44,23]
-mstr = "hello world"  #-> ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '%']
+# mlist = [1,23,44,23]
+# mstr = "hello world"  #-> ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '%']
 
 
 """
