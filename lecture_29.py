5e7aba8cd1a0fae06deee91e085e4cc7b5e6424b
Shoaib <mohammad.shoaib3786@gmail.com>
2023-10-07 14:57:36 +0530

    54 commit

--- lecture_29.py
+++ lecture_29.py
@@ -2,3 +2,132 @@
 OBJECT ORIENTED PROGRAMMING:> (OOPs concept)
 """
 
+# Classes and Objects
+
+"""BASIC STRUCTURE:"""
+
+
+class Person:
+
+    """Objective
+    1. User input
+    2. Making dictionary
+    """
+    def Gen_personal_info(self):
+        mdict = {}
+        # 1.
+        name = input('Enter your name: ')
+        age = int(float(input("Enter your age: ")))
+        dob = input('Enter your DOB: ')
+        gender = input('Enter your gender: ')
+
+        # 2.
+        mdict = {
+            'name': name,
+            'age': age,
+            'dob':dob,
+            'gender':gender
+        }
+
+        return mdict
+
+
+
+    """Objective:
+    1. Ask userinput which user want to fetch
+    2. Perfrom dictionary operation for fetching user desired query.
+    """
+    def User_query(self, mdict):
+
+        print('Pleas enter value from the given list: *[name, age, dob, gender]*')
+        dic_value = ['name', 'age', 'dob', 'gender']
+        
+        query = input("ENTER YOUR QUERY") # HELLO
+        
+        while True:
+
+            if (query in dic_value): #TRUE
+
+                # to show user desired query
+
+                value = mdict[query]
+
+                print(f'Your desired data: {value}')
+
+                break
+
+
+            else:
+                print('Try again')
+                query = input("Enter your query: ")
+
+
+
+# person_1 = Person()
+
+# mdict = person_1.Gen_personal_info()
+
+# person_1.User_query(mdict)
+
+# class vehicle:
+
+#     def car_info(lalit, self, car_name):
+
+#         print('colour of the car is: ', self)
+#         print('name of the car is: ', car_name)
+
+
+# obj1 = vehicle()
+
+# obj1.car_info('black', 'Alto')
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Write a Python program to replace the last value of tuples in a list.
+Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+Expected Output: [(10, 20, 100), (40, 50, 100), (70, 80, 100)]
+
+# PERFORM USING CLASSES AND OBJECTS 
+"""
+
+
+
+# class myClass:
+
+    # def changeNum(self,mlist):
+
+    #     mlst2 = []
+    #     for i in mlist:  # (10, 20, 40)
+
+    #         lst = list(i)  # typecasting
+
+    #         lst[-1] = 100
+
+    #         mtup = tuple(lst)  # typecasting
+
+    #         mlst2.append(mtup)
+        
+    #     return mlst2
+
+    
+#     def printList(self, color):
+    
+#         print(color)
+
+# obj1 = myClass()
+# obj2 = myClass()
+
+# obj1.printList('red')
+
+# obj2.printList('pink')
---
+++ lecture_30.py
@@ -0,0 +1,81 @@
+"""
+OPPs concept:
+1. Constructor -> (__init__()) 
+"""
+
+# class person:
+
+#     def __init__(self, myname, myage, mygender):
+
+#         self.name = myname
+#         self.age  = myage
+#         self.gender = mygender
+
+
+#     def mprint(self):   
+#         print(self)
+#         print('my name is: ', self.name)
+#         print('my age is: ', self.age)
+#         print('my gender is: ', self.gender)
+#         print('\n\n')
+
+
+# obj1 = person('max', 23, 'bhjbhujb')
+
+# obj1.mprint()
+
+# obj2 = person('lex', 30, 'Female')
+# obj2.mprint()
+
+
+
+
+# Exercise:
+"""
+Q1. Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.
+"""
+
+# class Circle:
+
+#     def __init__(self, mPi, myRadius):
+        
+#         self.pi = mPi
+#         self.r  = myRadius
+#         self.marea = 0
+#         self.mperi = 0
+
+#     def area(self):
+        
+#         self.marea = self.pi * self.r ** 2
+#         print(f'{self} -> area is: ',self.marea)
+
+
+#     def perimeter(self):
+        
+#         self.mperi = 2 * self.pi * self.r
+#         print(f'{self} -> perimeter is: ',self.mperi)
+
+
+# c1 = Circle(mPi= 3.14, myRadius= 2.6)
+# c2 = Circle(mPi= 3.14, myRadius= 3.5)
+
+
+# print('object 1 : ', c1)
+# print('object 2 : ', c2)
+
+# c1.area()
+
+# c2.area()
+
+# c1.perimeter()
+
+# c2.perimeter()
+
+
+
+
+
+
+"""
+Q2. Write a Python program to create a calculator class. Include methods for basic arithmetic operations.
+"""
