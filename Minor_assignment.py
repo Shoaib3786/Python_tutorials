ef198d7d35744412c721d29a0d63e3e6817ceeeb
Shoaib <mohammad.shoaib3786@gmail.com>
2023-10-10 13:57:12 +0530

    55 commit

---
+++ Minor_assignment.py
@@ -0,0 +1,34 @@
+"""Assigment on OOPs concept"""
+
+"""
+1. Make a python program as classes & objects.
+    - Make the class methods name it as "generateNum()" that generates any single number between (0 to 7).
+    - Make another class method name it as "getUserInput()" to ask user to enter their own number.
+    - Make another class method name it as "Evaluate()" to match both the numbers, 
+        -> if match found then print 'number is found' 
+        -> else print 'number not found'
+
+    test your class working by creating 2 different class instances(or can say objects)
+
+    
+2. Make the python programming as classes and objects.
+    - Make the class constructor that takes stores student_name, math_marks
+    - Make the class method name it as "stud_info()" that takes and builds the list that stores these variable values and returns that list.
+
+    - Now make the different instances of the class and pass studen_name and math_marks as argument to the constructor
+    - Make the different class instances as stud_01, stud_02 and stud_03 and then call stud_info().
+    - On stud_info() method by different instances of the class w'll get the list of each instances, now store that list into a dictionary as values and class instances name as key of that dictionary.
+
+    'Example:'
+    stud_01 = ['max', 78]
+    stud_02 = ['rohan', 99]
+    stud_03 = ['raman', 100]
+
+    Final Dictinary must look like:
+    stud_dict = {
+        'stud_01': ['max', 78],
+        'stud_02': ['rohan', 99],
+        'stud_03': ['raman', 100]
+    }
+
+"""
